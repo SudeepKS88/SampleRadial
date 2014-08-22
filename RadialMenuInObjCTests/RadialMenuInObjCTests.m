4c8d899a4ce65d78a82c0f893324f1d27c44f523
RapidValue <rapidvalue@RapidValues-MacBook-Pro-2.local>
2014-08-22 17:45:52 +0530

    test case

--- RadialMenuInObjCTests/RadialMenuInObjCTests.m
+++ RadialMenuInObjCTests/RadialMenuInObjCTests.m
@@ -32,11 +32,13 @@
     
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
+- (void)testCompareExample {
+
+    int valueX = 1;
+    int valueY = 1;
+    
+    XCTAssertTrue(valueX == valueY);
+
 }
 
 @end
